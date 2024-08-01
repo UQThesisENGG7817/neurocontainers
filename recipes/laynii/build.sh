cf8c675a9ab0754c0811902776144c2dea8e42a1
php1301 <luudanthanchet@gmail.com>
2024-08-01 13:51:27 +1000

    Switch to curl

--- recipes/laynii/build.sh
+++ recipes/laynii/build.sh
@@ -31,7 +31,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="mkdir -p ${mountPointList}"                      `# create folders for singularity bind points` \
    --install wget ca-certificates unzip \
    --workdir /opt/${toolName}-${toolVersion}/ \
-   --run="wget --secure-protocol=TLSv1_2 https://github.com/layerfMRI/LAYNII/releases/download/v2.2.1/LayNii_v${toolVersion}_Linux64.zip \
+   --run="curl -LO https://github.com/layerfMRI/LAYNII/releases/download/v2.2.1/LayNii_v${toolVersion}_Linux64.zip \
             && unzip LayNii_v${toolVersion}_Linux64.zip \
             && rm LayNii_v${toolVersion}_Linux64.zip" \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ \
