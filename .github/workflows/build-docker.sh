2e924efd62e99d3a1c1de2519632ded004924c15
php1301 <luudanthanchet@gmail.com>
2024-08-01 15:30:36 +1000

    Add rclone

--- .github/workflows/build-docker.sh
+++ .github/workflows/build-docker.sh
@@ -51,7 +51,7 @@ then
     sudo apt-get install -y software-properties-common
     sudo add-apt-repository -y ppa:apptainer/ppa
     sudo apt-get update
-    sudo apt-get install -y apptainer 
+    sudo apt-get install -y apptainer rclone
 fi
 
 
